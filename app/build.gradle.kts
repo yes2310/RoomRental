b62a8de2135838a836f1b1773543b6c3f9e23f8a
seonmae <jeongseonmae4@gmail.com>
2025-11-13 10:36:29 +0900

    Fix: Material Calendar View 의존성 충돌 해결
    AndroidX와 충돌하는 구버전 support library를 exclude하여
    duplicate class 오류 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,11 @@ dependencies {
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 
     // Material Calendar View
-    implementation("com.github.prolificinteractive:material-calendarview:2.0.1")
+    implementation("com.github.prolificinteractive:material-calendarview:2.0.1") {
+        exclude(group = "com.android.support", module = "support-compat")
+        exclude(group = "com.android.support", module = "support-core-utils")
+        exclude(group = "com.android.support", module = "support-annotations")
+    }
     implementation("com.jakewharton.threetenabp:threetenabp:1.4.6")
 
     implementation(libs.appcompat)
